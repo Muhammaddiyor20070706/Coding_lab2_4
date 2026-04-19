41b2636526e4287d834b5c89b6fc9967b750ba0c
Muham <546251100084@csu.uz>
2026-04-19 16:25:14 +0500

    hello

--- main.cpp
+++ main.cpp
@@ -1,49 +1,68 @@
 #include <iostream>
-//1-lab: Static umumiy qiymat (hamma obyekt uchun umumiy)
-//2-lab: Transport → Car
+// 1-lab: Animal → Dog, Cat (virtual speak())
+// 2-lab: Abstrakt Transport
 class Transport {
-    static int count;
     public:
-    Transport(){
-        count++;
+    Transport() = default;
+    virtual void Transporting_way()=0;
+    virtual double samaradorligi(double actual_distance,double real_distance)=0;
+    virtual ~Transport()=default;
+};
+class plane: public Transport {
+    public:
+    void Transporting_way() override {
+        std::cout<<"I fly over you! So I am plane you know."<<std::endl;
+    };
+    double samaradorligi(double actual_distance , double real_distance) override {
+        return actual_distance/real_distance*100;
+    };
+};
+class car : public plane {
+    public:
+    void Transporting_way() override {
+        std::cout<<"I run with you! So you know what, I am your car. "<<std::endl;
+    };
+    double samaradorligi(double actual_distance , double real_distance) override {
+        return actual_distance/real_distance*100;
+    };
+};
+
+class Animal {
+    public:
+    virtual void Speak(){
+        std::cout<<"Speak"<<std::endl;
     }
-    virtual ~Transport() {
-        count--;
-    }
-    virtual void transport(){
-        std::cout<<"Transportation function"<<std::endl;
-    }
-    static void getCount() {
-        std::cout<<"static Count:"<<count<<std::endl;
+
+};
+class Dog: public Animal {
+    public:
+    void Speak()override {
+        std::cout<<"Woof woof!"<<std::endl;
     }
 };
-int Transport::count = 0;
-class Car: public Transport {
+class Cat: public Animal {
     public:
-    void transport()override {
-        std::cout<<"Car only transports on road"<<std::endl;
-    }
-};
-class Ship: public Transport {
-    public:
-    void transport()override {
-        std::cout<<"Ship only transports on Sea"<<std::endl;
-    }
-};
-class Plane: public Transport {
-    public:
-    void transport()override {
-        std::cout<<"Plane only transports on Air"<<std::endl;
+    void Speak()override {
+        std::cout<<"Miaow miaow"<<std::endl;
     }
 };
 int main() {
-    Car car;
-    Car::getCount();
-    car.transport();
-    Ship ship;
-    Ship::getCount();
-    ship.transport();
-    Plane plane;
-    Plane::getCount();
-    plane.transport();
+    int choose = 0;
+    std::cout<<"Choose your choice:1->1-lab, 2-> Lab-2"<<std::endl;
+    std::cin>>choose;
+    if (choose==1) {
+        Dog dog1;
+        dog1.Speak();
+        Cat cat1;
+        cat1.Speak();
+    }else if (choose==2) {
+        plane plane1;
+        plane1.Transporting_way();
+        std::cout<<"samaradorligi:"<<plane1.samaradorligi(1700,2900)<<"%"<<std::endl;
+        car car1;
+        car1.Transporting_way();
+        std::cout<<"samaradorligi:"<<car1.samaradorligi(1700,1900)<<"%"<<std::endl;
+    }
+
+
 }
